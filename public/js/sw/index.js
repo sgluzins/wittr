b0dc7eda2bc3ce326637ca7aa40dc49aca995b42
sgluzins <sgluzins@gmail.com>
2018-01-20 22:21:22 -0800

    quiz 3.11

--- public/js/sw/index.js
+++ public/js/sw/index.js
@@ -1,6 +1,15 @@
 self.addEventListener('fetch', function(event) {
 	// TODO: respond to all requests with an html response
 	// containing an element with class="a-winner-is-me".
-	// Ensure the Content-Type of the response is "text/html"
+  // Ensure the Content-Type of the response is "text/html"
+  event.respondWith(
+    new Response(
+      '<div class="a-winner-is-me">hello!</div>',
+        { 
+          headers: {'content-type': 'text/html'}
+        }
+    )
+  )
+ 
   console.log(event.request);
 });
