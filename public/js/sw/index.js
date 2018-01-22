fd26970072f4362bdb33ea11fed1b7578c4ae1fd
sgluzins <sgluzins@gmail.com>
2018-01-21 20:52:55 -0800

    quiz 3.18

--- public/js/sw/index.js
+++ public/js/sw/index.js
@@ -16,4 +16,15 @@ self.addEventListener('install', function(event) {
 self.addEventListener('fetch', function(event) {
   // TODO: respond with an entry from the cache if there is one.
   // If there isn't, fetch from the network.
+
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      if(response != null){
+        return response;
+      }
+      else {
+        return fetch(event.request);
+      }
+    })
+  );
 });
